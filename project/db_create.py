f53b5c42aeb48723c7f88cb05213fd2a8ad697d1
KEVIN OBERLAG <koberlag@gmail.com>
2018-05-05 08:00:17 -0500

    Added foreign keys and migrate script

--- project/db_create.py
+++ project/db_create.py
@@ -2,15 +2,15 @@
 
 from views import db
 from models import Task
-from datetime import date
+from datetime import date, datetime
 
 
 # create the database and the db table
 db.create_all()
-
+datetime
 # insert data
-# db.session.add(Task("Finish this tutorial", date(2010, 9, 22), 10, 1))
-# db.session.add(Task("Finish Real Python", date(2010, 10, 3), 10, 1))
+db.session.add(Task("Finish this tutorial", date(2010, 9, 22), 10, datetime.utcnow(), 1, 1))
+db.session.add(Task("Finish Real Python", date(2010, 10, 3), 10, datetime.utcnow(), 1, 1))
 
 # commit the changes
 db.session.commit()
